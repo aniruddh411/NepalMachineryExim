0e54fb60fa2328f80665bc2930f6c915aa04e981
aniruddh411 <agrawalanirudh1375@gmail.com>
2021-06-20 14:44:44 +0545

    Nav Option
    White or Blue

---
+++ Navs/Blue/JS/Insibility.js
@@ -0,0 +1,26 @@
+var divs = ["Scaffold", "Other", "BarCutter", "BarBender", "StirrupBender", "SteelBarCutter", "GUTEetc", "Pneumatic", "Power", "Hand", "Hydraulic", "Municipal", "Ramming", "Lifting", "Ventilation", "Camps", "Electrical", "Welding", "Safety", "OtherM", "Kitchen"];
+var visibleDivId = null;
+
+function toggleVisibility(divId) {
+    if (visibleDivId === divId) {
+        //visibleDivId = null;
+    } else {
+        visibleDivId = divId;
+    }
+    hideNonVisibleDivs();
+}
+
+function hideNonVisibleDivs() {
+    var i, divId, div;
+    for (i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if (visibleDivId === divId) {
+            div.style.display = "flex";
+            div.style.justifyContent = "center";
+            div.style.alignItems = "center";
+        } else {
+            div.style.display = "none";
+        }
+    }
+}
